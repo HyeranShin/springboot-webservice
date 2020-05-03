d8ed06427d484f2be57f2a4c02332f4f018fed63
HyeranShin <hyeran9712@naver.com>
2020-05-04 04:53:08 +0900

    feat: 게시글 등록, 전체 조회 화면 작성 & 연결

--- src/main/java/com/study/springboot/service/PostsService.java
+++ src/main/java/com/study/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.study.springboot.service;
 
 import com.study.springboot.domain.posts.Posts;
 import com.study.springboot.domain.posts.PostsRepository;
+import com.study.springboot.web.dto.PostsListResponseDto;
 import com.study.springboot.web.dto.PostsResponseDto;
 import com.study.springboot.web.dto.PostsSaveRequestDto;
 import com.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +40,14 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) //트랜잭션 범위는 유지하되, 조회 기능만 남겨 조회 속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                //위의 코드는 다음과 같다. map(posts -> new PostsListResponseDto(posts))
+                //postRepository 결과로 넘어온 Posts의 Stream을 map을 통해 PostsListResponseDto 변환 -> List로 반환
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/study/springboot/web/IndexController.java
@@ -0,0 +1,25 @@
+package com.study.springboot.web;
+
+import com.study.springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) { //Moel: 서버 템플릿 엔진에서 사용할 수 있는 객체 저장
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}

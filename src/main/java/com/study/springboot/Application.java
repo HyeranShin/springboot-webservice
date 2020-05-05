99da4ac5a6756cb7dc7175a7fc6e083b5cbeb2e6
HyeranShin <hyeran9712@naver.com>
2020-05-05 18:02:27 +0900

    test: 기존 테스트에 시큐리티 적용

--- src/main/java/com/study/springboot/Application.java
+++ src/main/java/com/study/springboot/Application.java
@@ -2,10 +2,9 @@ package com.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //메인 클래스
-@EnableJpaAuditing //JPA Auditing 활성화
+//@EnableJpaAuditing //JPA Auditing 활성화 ---> @@WebMvcTest를 위해 JpaConfig 클래스로 분리 -> 일반적인 @Configuration은 스캔 X)
 @SpringBootApplication //스프링 부트, 스프링 Bean 읽기와 생성 자동 설정. 프로젝트의 최상단에 위치해야 함
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/study/springboot/web/HelloControllerTest.java
+++ src/test/java/com/study/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.study.springboot.web;
 
+import com.study.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,13 +16,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) //스프링 실행자 -> 스프링 부트 테스트와 JUnit 사이 연결자
-@WebMvcTest //Web(Spring MVC) 집중 테스트 -> @Controller, @ControllerAdvice 등 사용 가능)
+@WebMvcTest(controllers = HelloController.class, //Web(Spring MVC) 집중 테스트 -> @Controller, @ControllerAdvice 등 사용 가능)
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class) //스캔 대상에서 제거
+        })
 public class HelloControllerTest {
 
     @Autowired //스프링이 관리하는 빈(Bean) 주입
     private MockMvc mvc; //웹 API 테스트. 스프링 MVC 테스트의 시작점
 
     @Test
+    @WithMockUser(roles = "USER")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
@@ -29,6 +37,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
